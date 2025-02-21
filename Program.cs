6a9a3452469de7bcb04a1508619fd36e76e0c7c3
Fazliddin <proklyatdonatom2.0@gmail.com>
2025-02-21 18:57:34 +0500

    WeatherApp2

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿string city = Console.ReadLine();
+
+const string KEY = "1a798bad05f71da803740b14e9d0d166";
+
+using (HttpClient client = new())
+{
+    try
+    {
+        string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={KEY}";
+        HttpResponseMessage responce = await client.GetAsync(url);
+
+        if(responce.IsSuccessStatusCode)
+        {
+            string result = await responce.Content.ReadAsStringAsync();
+            Console.WriteLine(result);
+        }
+        else
+        {
+            Console.WriteLine($"Error: {responce.StatusCode}");
+        }
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine($"Exception: {ex.Message}");
+    }
+}
